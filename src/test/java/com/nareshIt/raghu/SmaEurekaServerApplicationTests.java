749652a7c89e81e3d925e5bb27ba681e326a7c88
Praveen <praveen-itschool@gmail.com>
2024-10-16 13:42:01 +0530

    first commit

---
+++ src/test/java/com/nareshIt/raghu/SmaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nareshIt.raghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
